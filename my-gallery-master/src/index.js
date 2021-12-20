746963006d199ecb5f98bd5925bd778a3e4d0e32
satellites7 <69025973+satellites7@users.noreply.github.com>
2021-12-20 18:50:48 +0800

    Add files via upload

---
+++ my-gallery-master/src/index.js
@@ -0,0 +1,26 @@
+// let photos = document.querySelectorAll('.photo');
+// let main = document.querySelector('main');
+// let pc = function (params) {
+//    for (let item of photos) {
+//     let width = item.width / 2
+//     let height = item.height / 2     
+//      item.width = width 
+//      item.height = height 
+//     } 
+// } 
+
+
+
+// let phone =function (params) {
+//     for (let item of photos) {
+//         let width = item.width / 2
+//         let height = item.height / 2         
+//          item.width = width / 2.74
+//          item.height = height / 2.74
+    
+//     }  
+// }
+
+// window.onload = pc()
+
+
